0292c14122a3c9d4d7c893ab1f00f3d5c0ab96b9
mkonji <mkonji@tuta.io>
2024-03-06 15:07:03 -0500

    display edit

--- opt_setdisplay.sh
+++ opt_setdisplay.sh
@@ -6,34 +6,34 @@ yellow='\033[1;33m'
 blue='\033[0;94m'
 reset='\033[0m'
 
-read -p "$(echo -e "${blue}Enter username: ${reset}")" user_name
+read -p "$(echo -e "${blue}enter username: ${reset}")" user_name
 
 read -p "$(echo -e "${blue}Is '${user_name}' the correct username? (y/n): ${reset}")" username_confirm
 if [[ $username_confirm != "y" && $username_confirm != "Y" ]]; then
-	echo -e "${red}Error: Username not confirmed.${reset}"
+	echo -e "${red}error: username not confirmed.${reset}"
 	exit 1
 fi
 
 if ! id -u "$user_name" &>/dev/null; then
-	echo -e "${red}Error: Username '${user_name}' not found.${reset}"
+	echo -e "${red}error: username '${user_name}' not found.${reset}"
 	exit 1
 fi
 
 home_dir="/home/$user_name"
 
-read -p "$(echo -e "${blue}Do you want to configure display resolution? (y/n): ${reset}")" configure_resolution
+read -p "$(echo -e "${blue}do you want to configure display resolution? (y/n): ${reset}")" configure_resolution
 if [ "$configure_resolution" != "y" ]; then
-	echo -e "${yellow}Skipping display resolution configuration.${reset}"
+	echo -e "${yellow}skipping display resolution configuration.${reset}"
 	exit 0
 fi
 
 echo -e "${green}available monitors:${reset}"
 xrandr | grep -w connected | awk '{print $1, $3}'
-read -p "$(echo -e "${blue}Enter the desired display resolution (e.g., 1920x1080): ${reset}")" resolution
+read -p "$(echo -e "${blue}enter the desired display resolution (e.g., 1920x1080): ${reset}")" resolution
 
-read -p "$(echo -e "${blue}Enter the desired refresh rate (e.g., 60): ${reset}")" refresh_rate
+read -p "$(echo -e "${blue}enter the desired refresh rate (e.g., 60): ${reset}")" refresh_rate
 
-read -p "$(echo -e "${blue}Enter the desired orientation (e.g., normal/left/right/inverted): ${reset}")" orientation
+read -p "$(echo -e "${blue}enter the desired orientation (e.g., normal/left/right/inverted): ${reset}")" orientation
 
 connected_monitors=$(xrandr | grep -w connected | awk '{print $1}')
 for monitor in $connected_monitors; do
@@ -41,21 +41,20 @@ for monitor in $connected_monitors; do
 done
 
 config_file="$home_dir/.config/screenlayout.sh"
-echo "#!usr/bin/usr bash" >$config_file
+echo "#!usr/bin/env bash" >$config_file
 echo "xrandr --output $(xrandr | grep -w connected | awk '{print $1}') --mode $resolution --rate $refresh_rate --rotate $orientation" >>$config_file
 chmod +x $config_file
 
 bat $home_dir/.config/screenlayout.sh
-echo -e "${blue}Does the display look okay with the new configuration? (y/n)${reset}"
+echo -e "${blue}does the display look okay with the new configuration? (y/n)${reset}"
 read confirm
 if [ "$confirm" != "y" ]; then
-	echo -e "${red}Reverting changes.${reset}"
+	echo -e "${red}reverting changes.${reset}"
 	exit 1
 fi
 
-bat $home_dir/.config/screenlayout.sh
-
-echo -e "${blue}Setting wallpaper.${reset}"
+##
+echo -e "${blue}setting wallpaper.${reset}"
 if ! nitrogen --set-zoom $home_dir/Pictures/wallpaper/wallpaper1.png; then
-	echo -e "${red}Error setting wallpaper.${reset}"
+	echo -e "${red}error setting wallpaper.${reset}"
 fi
