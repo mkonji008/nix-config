3ba48a7285468cdc133f2f7e8b13980a7b4f9816
mkonji <mkonji@tuta.io>
2024-03-25 14:59:06 -0400

    changes to i3

---
+++ dots/dotconfig/screenlayout/desktop-dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 3840x1080 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 960x1080 --rotate inverted --output HDMI-A-0 --off
