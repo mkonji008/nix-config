370edf26c26e452cda6f4bd5f4e8e39699077abf
mkonji008 <mkonji@tuta.io>
2024-04-24 12:16:02 -0400

    added zoxide to bash

--- dots/dotconfig/screenlayout/desktop-dual.sh
+++ dots/dotconfig/screenlayout/desktop-dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 3840x1080 --pos 0x0 --rate 144 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 917x1080 --rate 60 --rotate inverted --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 3840x1080 --pos 0x0 --rate 144 --primary --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 917x1080 --rate 60 --rotate inverted --output HDMI-A-0 --off
